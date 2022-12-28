af34eaf9cd45d47f31c0b34b872ae439093fa293
dambaliya381 <dharmishtha9879@gmail.com>
2022-12-28 21:29:07 +0530

    second commit

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,6 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2018 Brought To You By<a href="https://code-projects.org/">Code-Projects</a></strong>
+    <p>Smart Mall</p>
+    <!-- <strong>Copyright &copy; 2018 Brought To You By<a href="https://code-projects.org/">Code-Projects</a></strong> -->
 </footer>
