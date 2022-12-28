bfbbc373d72732d83201980fba07ca4af728c1cc
dambaliya381 <dharmishtha9879@gmail.com>
2022-12-28 20:16:16 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2018 Brought to You By <a href="https://code-projects.org/">Code-Projects</a></strong>
+    </div> -->
+    Smart mall system
+</footer>
